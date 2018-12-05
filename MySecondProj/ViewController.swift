622d89a285c9d8fdfdba8341fc794f1870717a09
jeong jun seo <jeongjunseo@jjuns-iMac.local>
2018-12-05 10:46:49 +0900

    1. 기존 BreakPoint 테스트 소스 막음. 2. viewDidAppear() 추가.

--- MySecondProj/ViewController.swift
+++ MySecondProj/ViewController.swift
@@ -16,13 +16,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var count = 0
-        for row in 5...10 {
-            count += 1
-            self.sum += row
-        }
+//        var count = 0
+//        for row in 5...10 {
+//            count += 1
+//            self.sum += row
+//        }
+//
+//        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
         
-        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
 
 
