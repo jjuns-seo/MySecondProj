31791b07eff1a6cac07174b623137890ba127858
jeong jun seo <jeongjunseo@jjuns-iMac.local>
2018-12-05 10:42:04 +0900

    Initial Commit

---
+++ MySecondProj/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  MySecondProj
+//
+//  Created by jeong jun seo on 05/12/2018.
+//  Copyright © 2018 jeong jun seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var sum = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        
+        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
+    }
+
+
+}
+
